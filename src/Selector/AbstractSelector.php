ade3af40849d7e3c708e5fec9b2496e368621da2
codealfa <sdmarshall73@gmail.com>
2024-06-09 08:31:13 -0400

    Match content ignoring space

--- src/Selector/AbstractSelector.php
+++ src/Selector/AbstractSelector.php
@@ -11,12 +11,12 @@ abstract class AbstractSelector implements SelectorInterface, Stringable
         return $this->render();
     }
 
-    protected function cssStripSlash($identifier): string
+    protected function cssStripSlash(string $identifier): string
     {
         return preg_replace("#\\\\([^0-9a-fA-F\r\n])#", '\1', $identifier);
     }
 
-    protected function getDelimiter($identifier): string
+    protected function getDelimiter(string $identifier): string
     {
         return str_contains($identifier, '"') ? "'" : '"';
     }
