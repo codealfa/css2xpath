9cf512505ddd58328ad0507b348663130338c05f
codealfa <samuel@jch-optimize.net>
2026-08-24 15:50:46 -0400

    Modernize css2xpath baseline

--- src/Collections/AttributeCollection.php
+++ src/Collections/AttributeCollection.php
@@ -1,15 +1,17 @@
 <?php
 
 /**
- * JCH Optimize - Performs several front-end optimizations for fast downloads
+ * CSS to XPath Converter
  *
- *  @package   jchoptimize/core
- *  @author    Samuel Marshall <samuel@jch-optimize.net>
- *  @copyright Copyright (c) 2025 Samuel Marshall / JCH Optimize
- *  @license   GNU/GPLv3, or later. See LICENSE file
+ * @package   codealfa\css2xpath
+ * @author    Samuel Marshall <samuel@jch-optimize.net>
+ * @copyright Copyright (c) 2026 Samuel Marshall / JCH Optimize
+ * @license   GNU/GPLv3, or later. See LICENSE file
  *
- *  If LICENSE file missing, see <http://www.gnu.org/licenses/>.
+ * If LICENSE file missing, see <https://www.gnu.org/licenses/>.
  */
+
+declare(strict_types=1);
 
 namespace CodeAlfa\Css2Xpath\Collections;
 
--- src/Selector/CssSelectorList.php
+++ src/Selector/CssSelectorList.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * CSS to XPath Converter
+ *
+ * @package   codealfa\css2xpath
+ * @author    Samuel Marshall <samuel@jch-optimize.net>
+ * @copyright Copyright (c) 2026 Samuel Marshall / JCH Optimize
+ * @license   GNU/GPLv3, or later. See LICENSE file
+ *
+ * If LICENSE file missing, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
 namespace CodeAlfa\Css2Xpath\Selector;
 
 use CodeAlfa\Css2Xpath\Collections\CssSelectorCollection;
